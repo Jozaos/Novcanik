25cf73559213530c021065c5386481cd15a38f0b
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-03-14 21:09:58 +0100

    Postavljanje smarterASP

--- Backend/Controllers/ExpenseController.cs
+++ Backend/Controllers/ExpenseController.cs
@@ -7,7 +7,7 @@ namespace Backend.Controllers
 
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class ExpenseController
+    public class ExpenseController:ControllerBase
     {
         private readonly WalletContext _context;
 
