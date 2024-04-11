6c95af50db16975be1842efd0694a4035c6c6044
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-04-11 20:18:09 +0200

    OVO NE VALJA JAKO

--- Backend/Controllers/GEController.cs
+++ Backend/Controllers/GEController.cs
@@ -1,64 +1,101 @@
-﻿using Backend.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Backend.Data;
+using Backend.Mapping;
 using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
-
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class GEController : ControllerBase
+    public class GEController : UniversalController<GE, GEDTORead, GEDtoInsertUpdate>
     {
-        private readonly WalletContext _context;
-
-        public GEController(WalletContext context)
+        public GEController(WalletContext context) : base(context)
         {
-            _context = context;
+            DbSet = _context.Ulovi;
+            _mapper = new GEMapping();
+
+
+        }
+        protected override void DeleteControl(GE entitet)
+        {
+
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        protected override List<GEDTORead> UcitajSve()
         {
-            return new JsonResult(_context.Group_Expenses.ToList());
+            var lista = _context.Ulovi
+                    .Include(g => g.Riba)
+                    .Include(g => g.Unos)
+
+
+                    .ToList();
+            if (lista == null || lista.Count == 0)
+            {
+                throw new Exception("Ne postoje podaci u bazi");
+            }
+            return _mapper.MapReadList(lista);
+        }
+        protected override GE KreirajEntitet(GEDtoInsertUpdate dto)
+        {
+            var Unos = _context.Unosi.FirstOrDefault(k => k.id == dto.GEUnos);
+            if (Unos == null)
+            {
+                throw new Exception("Ne postoji Unos s  id  " + dto.GEUnos + " u bazi");
+            }
+            var Riba = _context.Ribe.FirstOrDefault(k => k.id == dto.VrstaId);
+            if (Riba == null)
+            {
+                throw new Exception("Ne postoji Riba s  id  " + dto.VrstaId + " u bazi");
+            }
+
+            var entitet = _mapper.MapInsertUpdatedFromDTO(dto);
+            entitet.Riba = Riba;
+            entitet.Unos = Unos;
+
+            entitet.Tezina = dto.Tezina;
+            entitet.Duzina = dto.Duzina;
+            entitet.Kolicina = dto.Kolicina;
+            entitet.Fotografija = dto.Fotografija;
+
+            return entitet;
         }
 
-        [HttpGet]
-        [Route("{id:int}")]
-        public IActionResult GetBySifra(int id)
+
+
+        protected override GE NadiEntitet(int id)
         {
-            return new JsonResult(_context.Group_Expenses.Find(id));
+            return _context.Ulovi
+                           .Include(ulov => ulov.Unos)
+                           .Include(ulov => ulov.Riba)
+                           .FirstOrDefault(ulov => ulov.id == id)
+                   ?? throw new Exception("Ne postoji Ulov s šifrom " + id + " u bazi");
         }
 
-        [HttpPost]
-        public IActionResult Post(GroupExpense groupExpense)
-        {
-            _context.Group_Expenses.Add(groupExpense);
-            _context.SaveChanges();
-            return new JsonResult(groupExpense);
-        }
 
-        [HttpPut]
-        [Route("{id:int}")]
-        public IActionResult Put(int id, GroupExpense groupExpense)
+        protected override GE PromjeniEntitet(GEDtoInsertUpdate dto, GE entitet)
         {
-            var IdIzbaze = _context.Group_Expenses.Find(id);
-            IdIzbaze.account = groupExpense.account;
-            IdIzbaze.expense = groupExpense.expense;
+            var Unos = _context.Unosi.Find(dto.GEUnos) ?? throw new Exception("Ne postoji Unos s šifrom " + dto.GEUnos + " u bazi");
+            // ovdje je možda pametnije ići s ručnim mapiranje
+            var Riba = _context.Ribe.Find(dto.VrstaId) ?? throw new Exception("Ne postoji Riba s šifrom " + dto.VrstaId + " u bazi");
 
-            _context.Group_Expenses.Update(IdIzbaze);
-            _context.SaveChanges();
-            return new JsonResult(groupExpense);
-        }
+            entitet.Riba = Riba;
+            entitet.Unos = Unos;
+            entitet.Tezina = dto.Tezina;
+            entitet.Duzina = dto.Duzina;
+            entitet.Kolicina = dto.Kolicina;
+            entitet.Fotografija = dto.Fotografija;
 
-        [HttpDelete]
-        [Route("{id:int}")]
-        [Produces("application/json")]
-        public IActionResult Delete(int id)
-        {
-            var IdIzBaze = _context.Group_Expenses.Find(id);
-            _context.Group_Expenses.Remove(IdIzBaze);
-            _context.SaveChanges();
-            return new JsonResult(new { poruka = "Obrisano" });
+            return entitet;
         }
     }
+
+
+
 }
---
+++ Backend/Mappers/GEMapper.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Backend.Models;
+using System.Text.RegularExpressions;
+
+namespace Backend.Mapping
+{
+    public class GEMapping : Mapping<GE, GEDTORead, GEDtoInsertUpdate>
+    {
+
+        public GEMapping()
+        {
+            MapperMapReadToDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<GE, GEDTORead>()
+                .ConstructUsing(entitet =>
+                 new GEDTORead(
+                    entitet.id,
+
+                    entitet.Riba.Vrsta,
+                    entitet.Unos.id,
+                    entitet.Tezina,
+                    entitet.Kolicina,
+                    entitet.Duzina,
+                    entitet.Fotografija
+                    ));
+            }));
+
+            MapperMapInsertUpdatedFromDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<GEDtoInsertUpdate, GE>();
+            }));
+
+            MapperMapInsertUpdateToDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<GE, GEDtoInsertUpdate>()
+                .ConstructUsing(entitet =>
+                 new GEDtoInsertUpdate(
+
+
+                    entitet.Riba.id,
+                    entitet.Unos.id,
+                    entitet.Tezina,
+                    entitet.Kolicina,
+                    entitet.Duzina,
+                    entitet.Fotografija));
+            })); ; ;
+        }
+    }
+}
+
+
+
+
+
