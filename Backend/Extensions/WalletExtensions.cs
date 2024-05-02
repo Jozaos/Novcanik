9a22b29c387848aa55ee4bf8e48a70f711856961
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-05-02 08:45:23 +0200

    Dokumentacija

---
+++ Backend/Extensions/WalletExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Backend.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Backend.Extensions
+{
+    public static class WalletExtensions
+    {
+
+        public static void AddWalletSwaggerGen(this IServiceCollection Services)
+        {
+            // prilagodba za dokumentaciju, čitati https://medium.com/geekculture/customizing-swagger-in-asp-net-core-5-2c98d03cbe52
+
+            Services.AddSwaggerGen(sgo =>
+            { // sgo je instanca klase SwaggerGenOptions
+              // čitati https://devintxcontent.blob.core.windows.net/showcontent/Speaker%20Presentations%20Fall%202017/Web%20API%20Best%20Practices.pdf
+                var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Wallet API",
+                    Version = "v1",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "rebolj.josip031@gmail.com",
+                        Name = "Josip Rebolj"
+                    },
+                    Description = "Documentation for Wallet API",
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                    {
+                        Name = "Educational licence"
+                    }
+                };
+                sgo.SwaggerDoc("v1", o);
+
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+            });
+
+        }
+
+
+        public static void AddWalletCORS(this IServiceCollection Services)
+        {
+            // Svi se od svuda na sve moguće načine mogu spojitina naš API
+            // Čitati https://code-maze.com/aspnetcore-webapi-best-practices/
+
+            Services.AddCors(opcije =>
+            {
+                opcije.AddPolicy("CorsPolicy",
+                    builder =>
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+
+            });
+
+        }
+    }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,5 @@
 using Backend.Data;
+using Backend.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,16 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(opcije =>
-{
-    opcije.AddPolicy("CorsPolicy",
-        builder =>
-            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-    );
-
-});
+builder.Services.AddWalletSwaggerGen();
+builder.Services.AddWalletCORS();
 
 
 builder.Services.AddDbContext<WalletContext>(o =>
@@ -41,6 +34,7 @@ var app = builder.Build();
 //}
 
 
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
