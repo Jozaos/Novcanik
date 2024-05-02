84addb760245503b619de6df14cc86350854a2d0
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-05-02 09:12:57 +0200

    x

--- Backend/Controllers/AccountController.cs
+++ Backend/Controllers/AccountController.cs
@@ -21,31 +21,7 @@ namespace Backend.Controllers
         {
             DbSet = _context.Accounts;
         }
-        [HttpGet]
-        [Route("traziStranicenje/{stranica}")]
-        public IActionResult TraziPolaznikStranicenje(int stranica, string uvjet = "")
-        {
-            var poStranici = 8;
-            uvjet = uvjet.ToLower();
-            try
-            {
-                var accounts = _context.Accounts
-                    .Where(p => EF.Functions.Like(p.username.ToLower(), "%" + uvjet + "%")
-                                || EF.Functions.Like(p.owner_name.ToLower(), "%" + uvjet + "%"))
-                    .Skip((poStranici * stranica) - poStranici)
-                    .Take(poStranici)
-                    .OrderBy(p => p.username)
-                    .ToList();
 
-
-                return new JsonResult(_mapper.MapReadList(accounts));
-
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
         protected override void DeleteControl(Account entitet)
         {
          
